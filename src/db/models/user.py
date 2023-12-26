354e422ac2c583aac649fb645698a9c19144ef6b
Alfredo Martinez <amartinez2@integralads.com>
2023-12-25 20:31:29 -0600

    created users dal with no validations

--- src/db/models/user.py
+++ src/db/models/user.py
@@ -5,12 +5,6 @@ from sqlalchemy import Column, String, Integer, Date, Float
 class User(Base):
     __schema__ = 'prod'
 
-    name = Column(String)
-    height_cm = Column(Integer)
-    weight_kg = Column(Float)
-    birth_date = Column(Date)
-    image_url = Column(String)
-    vertical_image_url = Column(String)
-    feet = Column(Integer)
-    bottoms = Column(Integer)
-    tops = Column(Integer)
+    username = Column(String)
+    profile_id = Column(Integer)
+    password_hash = Column(String)
